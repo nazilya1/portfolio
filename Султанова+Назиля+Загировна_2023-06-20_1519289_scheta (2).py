b43771e767d72c2af1a5aeffe2786aa02ae9701c
nazilya1 <nazilazagirovna@gmail.com>
2023-06-24 00:20:53 +0300

    Мои успехи

---
+++ Султанова+Назиля+Загировна_2023-06-20_1519289_scheta (2).py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+s=Service('C:/Users/79151/Downloads/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+driver.get("https://idemo.bspb.ru/auth?response_type=code&client_id=1&redirect_uri=https%3A%2F%2Fidemo.bspb.ru%2Flogin%2Fsuccess&prefetch_uri=https%3A%2F%2Fidemo.bspb.ru%2Flogin%2Fprefetch&force_new_session=true")
+time.sleep(5)
+driver.set_window_size(1366,768)
+driver.find_element(By.NAME, "username").click()
+driver.find_element(By.NAME, "password").click()
+driver.find_element(By.ID, "login-button").click()
+driver.find_element(By.ID, "login-otp-button").click()
+driver.find_element(By.ID, "accounts-index").click()
+driver.find_element(By.XPATH, "//*[@id=\"contentbar\"]/form/div/a").click()
+driver.find_element(By.NAME, "condition.newAccountConditions").click()
+driver.find_element(By.ID, "submit").click()
+
